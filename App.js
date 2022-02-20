b24e27ab8d277789f0e78a782ff624a9c2a7361b
loicbdev <loicbodolec@msn.com>
2022-02-20 17:58:05 +0100

    init: bottom navigation

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import ProjectsList from './src/screens/ProjectsList';
+import Profile from './src/screens/Profile';
 
-import AppNavigator from './src/navigation/AppNavigator';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Projects') {
+              iconName = focused ? 'list' : 'list-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray'
+        })}
+      >
+        <Tab.Screen name="Projects" component={ProjectsList} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/screens/ProjectsList.js
+++ src/screens/ProjectsList.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 function ProjectsList(props) {
   const { navigation } = props;
   return (
     <View style={styles.container}>
       <Text style={styles.boldText}>Projects List</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
-        <Text style={styles.boldText}>Go to Detail</Text>
-      </TouchableOpacity>
     </View>
   );
 }
